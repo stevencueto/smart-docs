554d2d945ca6bce64d5df1b8fa8e18c7777651d0
stevencueto <steven.cueto1305@gmail.com>
2022-04-21 01:17:12 -0400

    text editor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import React from 'react';
+import WebsiteContainer from './components/WebsiteContainer';
 function App() {
   return (
     <div className="App">
+      <WebsiteContainer/>
     </div>
   );
 }
---
+++ src/components/Docs/DocsEditor.jsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useEffect, useState, Suspense} from "react"
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { io } from "socket.io-client"
+
+;import { EditorState } from 'draft-js';
+import { useParams } from "react-router-dom"
+import { Editor } from 'react-draft-wysiwyg';
+const DocsEditor = (props) => {
+    const { id } = useParams()
+    const [socket, setSocket] = useState()
+    const [show, setShow] = useState(false)
+    const [editorState, setEditorState] = React.useState(
+      () => EditorState.createEmpty(),
+    );
+    const handleEditorChange = (state) => {
+        setEditorState(state);
+    }
+    useEffect(()=>{
+      setShow(!show)
+    }, [])
+  return <>
+  {show &&
+        <Editor 
+        editorState={editorState}
+        state={editorState}
+        
+        onEditorStateChange={handleEditorChange}
+        placeholder="Write something!"
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        /> }
+  </>
+      }
+export default DocsEditor;
