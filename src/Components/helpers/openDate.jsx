8521b2341babfcca8daf8b30b858539e30277728
stevencueto <steven.cueto1305@gmail.com>
2022-04-27 01:30:31 -0400

    get date test

--- src/Components/helpers/openDate.jsx
+++ src/Components/helpers/openDate.jsx
@@ -1,4 +1,28 @@
-export const openDate = (date, APIcall) =>{
+import docsLink from "./docsAPI";
+
+const getDoc = async (id)=>{
+    let doc;
+    try {
+        const req = await fetch(`${docsLink}doc/data/${id}`, {
+            headers: {
+                'x-access-token': localStorage.getItem('docs-token'),
+            }
+          })
+        const res = await req.json()
+        if(res.success){
+            doc = res.data
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    return doc
+}
+
+
+export const openDate = (id) =>{
+    const doc = getDoc(id)
+    console.log(doc)
+    if(doc === null) return
     const now = new Date();
     const year = now.getFullYear();
     let month = now.getMonth() + 1; // Months start at 0!
@@ -7,6 +31,7 @@ export const openDate = (date, APIcall) =>{
     let rn = new Date(Date.UTC(year, month, day))
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     today = rn.toLocaleDateString('en-US', options)
-    if(today === date) return
-    APIcall()
+    if(today === doc?.openDate)return
 }
+
+
