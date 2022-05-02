d1f27d705bf93e1114e0e38cc0e031f806ff1125
stevencueto <steven.cueto1305@gmail.com>
2022-05-02 13:22:04 -0400

    commit user

--- src/Components/profile/Profile.jsx
+++ src/Components/profile/Profile.jsx
@@ -1,14 +1,12 @@
-import {useEffect, useContext} from 'react'
+import {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
-import UserContext from '../../context/UserContex'
 import Content from './Content'
 import SelectMenu from './ProfilePic'
 
 export default function Profile(props) {
   let navigate = useNavigate()
-  const {user} =useContext(UserContext)
   useEffect(()=>{
-    if(!localStorage.getItem('docs-token'))navigate('/', {replace: true})
+    if(!localStorage.getItem('docs-token'))navigate('/register', {replace: true})
   }, [])
   return (
     <section className='mt-20'> 
